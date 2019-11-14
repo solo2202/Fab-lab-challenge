bb9a8bef968850cf64a5e0944aa494a8897e9145
solo2202 <42084704+solo2202@users.noreply.github.com>
2019-11-14 23:42:35 +0530

    python code

---
+++ python-code/anidiot.py
@@ -0,0 +1,49 @@
+from socket import *
+
+
+HOST = "ip-address"
+PORT = 9005
+
+s = socket(AF_INET, SOCK_STREAM)
+print("1")
+s.bind((HOST, PORT))
+print("2")
+s.listen(1)
+conn, addr = s.accept()
+print(addr)
+print "Connected by: ", addr
+while True:
+    data = conn.recv(1024)
+
+    data.rstrip()
+
+    if data == "u":
+        GPIO.output(5, True)
+        GPIO.output(6, False)
+        GPIO.output(13, True)
+        GPIO.output(19, False)
+    elif data == 'l':
+        GPIO.output(5, True)
+        GPIO.output(6, False)
+        GPIO.output(13, False)
+        GPIO.output(19, True)
+    elif data == 'r':
+        GPIO.output(5, False)
+        GPIO.output(6, True)
+        GPIO.output(13, False)
+        GPIO.output(19, True)
+    elif data == 'd':
+        GPIO.output(5, False)
+        GPIO.output(6, True)
+        GPIO.output(13, False)
+        GPIO.output(19, True)
+    else :
+        GPIO.output(5, False)
+        GPIO.output(6, False)
+        GPIO.output(13, False)
+        GPIO.output(19, False)
+
+    print "Received: ", repr(data)
+    reply = ("ok")
+    conn.sendall(reply)
+conn.close()
